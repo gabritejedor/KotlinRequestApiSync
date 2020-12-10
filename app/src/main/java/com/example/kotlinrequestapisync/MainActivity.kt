ee3083918faaea8a8a3218fae46020154904e049
gabritejedor <tejedor.gabriel@gmail.com>
2020-12-10 15:10:31 +0100

    Genero cabecera y tema

--- app/src/main/java/com/example/kotlinrequestapisync/MainActivity.kt
+++ app/src/main/java/com/example/kotlinrequestapisync/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kotlinrequestapisync
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinrequestapisync.Services.WeatherApiService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -9,14 +12,28 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar : Toolbar = findViewById<Toolbar>(R.id.toolbar_main)
+
+
+        setSupportActionBar(toolbar)
         button.setOnClickListener {
             getApiValue()
         }
+
     }
+    override fun onCreateOptionsMenu(menu : Menu) : Boolean{
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
+        return true
+
+    }
+
     private fun getApiValue()
     {
         var respuesta = ""
